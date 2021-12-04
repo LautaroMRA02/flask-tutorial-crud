69c6f09d2a3c831ad89da7fd1cc78a7856f3683f
Lautaro Rivadero <lautaro.mra.2002@gmail.com>
2021-12-04 15:01:22 -0300

    code restructuring and update, edit def

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from flask import render_template,request,redirect
-
 from flaskext.mysql import MySQL
-
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 mysql = MySQL()
@@ -28,10 +27,7 @@ def index():
 	empleados=cursor.fetchall()
 	forData(empleados)
 	conn.commit()
-
 	return render_template('empleados/index.html', empleados=empleados)
-
-
 @app.route('/destroy/<int:id>')
 def destroy(id):
 	conn = mysql.connect()
@@ -39,37 +35,46 @@ def destroy(id):
 	cursor.execute("DELETE FROM `cac_actividad_empleados`.`empleados` WHERE id=%s", (id))
 	conn.commit()
 	return redirect('/')
-
 @app.route('/create')
 def create():
  return render_template('empleados/create.html')
-
 @app.route('/store', methods=['POST'])
 def storage():
 	_nombre=request.form['txtNombre']
 	_correo=request.form['txtCorreo']
-
 	_foto=request.files['txtFoto']
 	now= datetime.now()
 	tiempo= now.strftime("%Y%H%M%S")
 	if _foto.filename!='':
 		nuevoNombreFoto=tiempo+_foto.filename
 		_foto.save("uploads/"+nuevoNombreFoto)
-
-
 	sql = "INSERT INTO `cac_actividad_empleados`.`empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, %s, %s, %s);"
 	datos=(_nombre,_correo,nuevoNombreFoto)
-
-
 	conn = mysql.connect()
 	cursor = conn.cursor()
 	cursor.execute(sql,datos)
 	conn.commit()
-
 	return render_template('empleados/index.html')
-
-
-
-
+@app.route('/edit/<int:id>')
+def edit(id):
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	cursor.execute("SELECT * FROM `cac_actividad_empleados`.`empleados` WHERE id=%s", (id))
+	empleados=cursor.fetchall()
+	conn.commit()
+	return render_template('empleados/edit.html', empleados=empleados)
+@app.route('/update', methods=['POST'])
+def update():
+	_nombre=request.form['txtNombre']
+	_correo=request.form['txtCorreo']
+	_foto=request.files['txtFoto']
+	id=request.form['txtID']
+	sql = "UPDATE `cac_actividad_empleados`.`empleados` SET `nombre`=%s, `correo`=%s WHERE id=%s;"
+	datos=(_nombre,_correo,id)
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	cursor.execute(sql,datos)
+	conn.commit()
+	return redirect('/')
 if __name__ == '__main__':
 	app.run(debug=True)
